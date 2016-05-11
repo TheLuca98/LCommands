da7308c440921ae7d9810dff309e7d357819b225
Luca <TheLuca98@users.noreply.github.com>
2016-05-11 17:54:42 +0200

    Temporary permission fix
    Bukkit seems to ignore Command#setPermission(String). Fixed temporarily
    by adding a check before executing.

--- src/main/java/luca/plugins/cmd/BukkitExecutor.java
+++ src/main/java/luca/plugins/cmd/BukkitExecutor.java
@@ -20,8 +20,10 @@ package luca.plugins.cmd;
 
 import luca.plugins.cmd.exceptions.CommandException;
 import luca.plugins.cmd.exceptions.UsageException;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permission;
 
 import java.util.Arrays;
 import java.util.logging.Level;
@@ -33,24 +35,30 @@ public class BukkitExecutor extends Command {
     private final CustomCommand command;
 
     public BukkitExecutor(CustomCommand command) {
-        super(command.getName(), command.getPermission(), null, Arrays.asList(command.getAliases()));
+        super(command.getName());
+        setAliases(Arrays.asList(command.getAliases()));
         setDescription(command.getDescription());
         setUsage("/" + command.getName() + " " + command.getUsage());
+        setPermission(command.getPermission());
         setPermissionMessage(RED + LCommands.getMP().getMessage("error.command.permission"));
         this.command = command;
     }
 
     @Override
     public boolean execute(CommandSender sender, String label, String[] args) {
-        try {
-            command.execute(sender, new CommandArgs(args));
-        } catch (CommandException e) {
-            sender.sendMessage(e.getChatMessage());
-        } catch (UsageException e) {
-            sender.sendMessage(RED + LCommands.getMP().formatMessage("error.command.usage", getUsage()));
-        } catch (Throwable e) {
-            sender.sendMessage(RED + LCommands.getMP().getMessage("error.command.internal"));
-            LCommands.getInstance().getLogger().log(Level.SEVERE, null, e);
+        if (sender.hasPermission(getPermission())) {
+            try {
+                command.execute(sender, new CommandArgs(args));
+            } catch (CommandException e) {
+                sender.sendMessage(e.getChatMessage());
+            } catch (UsageException e) {
+                sender.sendMessage(RED + LCommands.getMP().formatMessage("error.command.usage", getUsage()));
+            } catch (Throwable e) {
+                sender.sendMessage(RED + LCommands.getMP().getMessage("error.command.internal"));
+                LCommands.getInstance().getLogger().log(Level.SEVERE, null, e);
+            }
+        } else {
+            sender.sendMessage(getPermissionMessage());
         }
         return true;
     }
